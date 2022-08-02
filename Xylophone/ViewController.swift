a4feed976b7e8bbcdbcfdca03183784dca91454d
egaraz <egaraz@netinfo.md>
2022-08-02 16:33:14 +0300

    implement the logic

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,39 @@
 //
 
 import UIKit
+import AVFoundation
+
+fileprivate let soundFormat : String = "wav"
 
 class ViewController: UIViewController {
 
+    var player : AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        guard let soundName = sender.titleLabel?.text else { return }
+        
+        playSound(soundName, format: soundFormat)
+    }
     
+    private func playSound(_ soundName: String, format: String) {
+        guard let url = Bundle.main.url(forResource: soundName, withExtension: format)
+        else { return }
+        
+        do {
+            
+            player = try AVAudioPlayer(contentsOf: url)
+            
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 
 }
 
